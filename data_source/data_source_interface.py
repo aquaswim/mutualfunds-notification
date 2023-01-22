159a58251ff8eb8342b14d3f583813a816ef6ace
Bayu Setiawan <bayusetiawan37@gmail.com>
2023-01-22 11:31:22 +0700

    remove all code

--- data_source/data_source_interface.py
+++
@@ -1,7 +0,0 @@
-from entities.mutual_fund import MutualFund
-
-
-class DataSourceInterface:
-    def get_mutual_funds_data(self, product_id: str) -> MutualFund:
-        """GET MUTUAL FUND DATA"""
-        pass
